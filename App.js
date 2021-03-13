a48a647206eaf45e8ae5195d66888fcc798ed8a9
Kyle Humphreys <kyle_humphreys@hotmail.co.uk>
2021-03-13 15:09:47 +0000

    fixing issue caused by importing a renamed component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import {startRoute, Map} from "./components/startroute/maps.js";
+import { SafeAreaView, StyleSheet, Text, View, Dimensions } from 'react-native';
+import {StartRoute, Map} from "./components/startroute/maps.js";
 
-export default startRoute; 
+export default StartRoute; 
