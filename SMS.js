2372d58ed87a61b72f40ae7105bf5f0a31e02d91
Benjamin Laczkovics <fekete.laczkovics@gmail.com>
2021-03-13 12:20:01 +0000

    Add files via upload
    Substring replaced with loops. Now it works for all latitudes and longitudes

--- SMS.js
+++ SMS.js
@@ -23,14 +23,40 @@ function SendSMS(phoneNumber){
       text = errorMsg;
     } else if (location) {
       text = JSON.stringify(location);
+      var lng;
+      var lat;
+
+      for (let i = 0; i < text.length-9; i++) {
+        if (text.substring(i, i+9) == "longitude") {
+          for (let j = i; j < text.length-18; j++) {
+            if (text.substring(j, j+1) == ",") {
+              lng = text.substring(i, j);
+              break;
+            }
+          }
+          break;
+        }
+      }
   
+      for (let i = 0; i < text.length-8; i++) {
+        if (text.substring(i, i+8) == "latitude") {
+          for (let j = i; j < text.length-16; j++) {
+            if (text.substring(j, j+1) == ",") {
+              lat = text.substring(i, j);
+              break;
+            }
+          }
+          break;
+        }
+      }
+      
       const accountSid = "AC310bc9979e3f631c0b755fd3ae9ad711";
       const authToken = "7cae1329d22dc7f789ffbf2855b90076";
       const client = require('twilio')(accountSid, authToken);
   
       client.messages
         .create({
-          body: 'http://maps.google.com/?q=' + text.substring(22, 30) + ',' + text.substring(44, 53),
+          body: 'http://maps.google.com/?q=' + lat + ',' + lng,
           from: '+447782588090',
           to: phoneNumber
         })
